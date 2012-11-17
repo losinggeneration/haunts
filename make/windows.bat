ac9d64faf13f81ad8c470b0c82d5ea35d8ee8162
Michael Lewis <foo@bar.xyz>
2012-11-17 09:32:54 -0800

    made windows batch file copy all the correct files

--- make/windows.bat
+++ make/windows.bat
@@ -3,11 +3,23 @@ cd tools
 go run version.go
 cd ..
 
-go build .
+REM go build .
 
-rm -rf \haunts_app
-mkdir \haunts_app\openme
-mv haunts.exe \haunts_app\openme
-cp -r data\* \haunts_app
+@REM rm -rf \haunts_app
+@REM mkdir \haunts_app\openme
+copy haunts.exe \haunts_app\openme\haunts.exe
 copy lib\windows\* \haunts_app\openme
-copy lib\windows\* \haunts_app\openme
+copy ..\fmod\lib\windows\* \haunts_app\openme
+
+@REM the \data dir gets flattened in the installation dir
+@ECHO OFF
+ECHO Copying data directories. 
+ECHO You can now cancel (CTRL-C) if nothing has changed.
+cd data
+cp *.json \haunts_app
+for /d %%I IN (*) DO (
+echo %%I
+cp -r %%I \haunts_app
+)
+
+cd ..
